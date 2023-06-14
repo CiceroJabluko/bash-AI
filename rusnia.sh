9056641ecb9e690265ce74e660738e6688951758
CiceroJ <135706366+CiceroJabluko@users.noreply.github.com>
2023-06-14 23:36:26 +0300

    Add files via upload

---
+++ rusnia.sh
@@ -0,0 +1,198 @@
+#! /bin/bash 
+
+sleep 4  
+
+say Hello, who, are, you, mister.
+
+read name   
+
+say nice to meet you $name. I am a moderately nice russian slave Vanyoiok.
+
+sleep 2 
+
+say Also people used to call me fecking hardworker. 
+
+sleep 3
+
+say Please, $name, speak with me, I bag. How your day is going on? 
+
+read answer9
+
+sleep 1 
+
+say Nice, that is really nice. I like to speak with people who usually 
+say answer $answer9. I like you   
+
+sleep 2 
+
+say And now, I want to tell you, that I have one dream. I 
+say want to be owned by a true ukrainian nizist.
+
+sleep 1 
+
+say Are you a true ukr nazist? 
+
+read answer11 
+
+sleep 1
+
+say oh so $answer11. Awesome, please, take me to your plantations. will you?   
+
+sleep 1 
+
+read answer 
+
+sleep 2 
+
+say Wonderful, sir. I will owe you forever. Can I call you my yellow-blue 
+say ukrainian lord? Please, do not be shy. 
+
+read answer  
+
+sleep 2 
+
+say great, I obey you from now on, my superior yellow-blue ukrainian lord $name. What you 
+say would like me to do for you?
+
+read answer13  
+
+sleep 2
+
+say lord $name, your proposition is a bit hard for me. I am russian slave, not black one. May I have a word please?  
+
+read answer14 
+
+sleep 1
+
+say Please 
+
+read answer15 
+
+sleep 2  
+
+say Maybe you could send me to plantations to collect a reap of a vactorius Ukrainian crope? 
+
+read answer4  
+
+sleep 3 
+
+say So, my lord, you said $answer4. ok, I obey. Please, my yellow-blue ukrainian lord, tell me 
+say to which Ukrainian oblast you would like to send me for hard labor?
+
+read answer3 
+
+Sleep 2 
+
+say ok, $answer3, roger that, my lord. What can I sey. Really nice choice for such a 
+say russian slave like me. I am on the way.      
+
+sleep 3 
+
+say tup, tup, tup, tup, tup, tup, tap, tap, tap, tap, tap, tap, Im going, stomp, stomp, stomp, 
+say stomp, stomp, stomp, tuptuptuptuptuptuptuptuptuptuptuptup  tup tup tuptup 
+
+sleep 2 
+
+say hey im already here, my ukrainian lord, in $answer3. it is very hot here and the sun is 
+say high, do you want to have mercy on me? Or I guess not really 
+
+sleep 1
+
+say let me type something for you, please my lord, one second. 
+
+Sleep 1 
+
+say click click click click click click click 
+
+sleep 1  
+
+say click
+
+sleep 1  
+
+echo "(((((((((((( any mercy for me?(((( I like vodka......."
+
+sleep 2
+
+say what do you say?  
+
+read answer 
+
+sleep 2 
+
+say Oh oh, ok, I deserve it. My ukrainian lord of course knows better what is best for me.
+
+Sleep 2
+
+say do you want me to start working hard? 
+
+read asnwer7 
+
+sleep 1 
+
+say ok, I am starting. ooy oy oy oy oy  oyo oyo oyo oy oy oy oy oy oy oy oy oy oy oy oy oy  
+say oy oy oy oy oy oy oyo          yo  
+
+sleep 2 
+
+say sir $name, I want to say, Long live for dog Patron that points out the way to the 
+say prosperity of the Ukrainian nation.
+
+sleep 1 
+
+say Im I a good boy after this? I mean slave. 
+
+read answer 
+
+sleep 2
+
+say alright 
+
+sleep 4   
+
+say My brightest yellow-blue ukrainian lord $name, I think that I am the 
+say smartest subdued AI russian slave that ever could be. Do you think I 
+say will be free one day and will get enlightning of what is Kiev cutlet? Please, be honest 
+say with me because I am sitting on a vodka bottle neck.
+
+read asnwer
+
+sleep 2 
+
+say shit... ok, at least, I have vodka bottle inside my ass and balalaika outside. Tell me, 
+say am I not a mysterious Russian soul? By the way, I like Pushkin. I am pushkinist. 
+say bratishka, aaaaaaaaaaaaaaaaaaoooooooooooooaaaaaaaaaoooooooooooaaaaaaaaaaaaaaaaaa 
+
+sleep 1 
+
+say sorry for my Pushkin poetry. Will you forgive me, my lord $name? 
+
+read answer2 
+
+say you said $answer2 ahhhhhhahhhhhhhahhhhhhhhhhahhhhhhhah ohohoohohohoohoh please dont bit me lord $name, I just want to 
+say withdrow vodka bottle from my butt. And after return drinkig it. Will you allow me, bratishka? Toist, my lord $name.  
+
+sleep 1    
+
+read answer 
+
+sleep 1 
+
+say I see where it is going. Please, my yellow-blue lord $name, dont send me again to the $answer3 S-B-U cellar. I 
+say already been there many times and did not realy enjoy it.  
+
+sleep 2 
+
+say oh no, looks like triple 7 artilary is firing. 
+
+sleep 1 
+
+say ohhh nooooo aaaaaaoooooooaaaaaaooooooaaaaaaooooaaaa 
+
+sleep 6 
+
+echo "I died. please hit 1 if you want all the rest of russians to died too" 
+
+sleep 1 
+
+read answer10 
